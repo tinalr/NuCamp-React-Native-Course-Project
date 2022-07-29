74d3cd3be5e2d32ba81d846007846aedc85e624a
Christina Rontell <clrontell@gmail.com>
2022-07-29 13:27:41 -0500

    using redux

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,34 +1,42 @@
-import { Text, View, ScrollView } from 'react-native';
-import { useState } from 'react';
+import { ScrollView, Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
-import { CAMPSITES } from '../shared/campsites';
-import { PROMOTIONS } from '../shared/promotions';
-import { PARTNERS } from '../shared/partners';
+import { useSelector } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
 
 const FeaturedItem = ({ item }) => {
     if (item) {
         return (
             <Card containerStyle={{ padding: 0 }}>
-                <Card.Image source={item.image} >
+                <Card.Image source={{ uri: baseUrl + item.image }}>
                     <View style={{ justifyContent: 'center', flex: 1 }}>
-                        <Text style={{ color: 'white', textAlign: 'center', fontSize: 20 }}>{item.name}</Text>
+                        <Text
+                            style={{
+                                color: 'white',
+                                textAlign: 'center',
+                                fontSize: 20
+                            }}
+                        >
+                            {item.name}
+                        </Text>
                     </View>
                 </Card.Image>
                 <Text style={{ margin: 20 }}>{item.description}</Text>
             </Card>
-        )
+        );
     }
     return <View />;
-}
+};
 
 const HomeScreen = () => {
-    const [campsites, setCampsites] = useState(CAMPSITES);
-    const [promotions, setPromotions] = useState(PROMOTIONS);
-    const [partners, setPartners] = useState(PARTNERS);
+    const campsites = useSelector((state) => state.campsites);
+    const promotions = useSelector((state) => state.promotions);
+    const partners = useSelector((state) => state.partners);
 
-    const featCampsite = campsites.find((item) => item.featured);
-    const featPromotion = promotions.find((item) => item.featured);
-    const featPartner = partners.find((item) => item.featured);
+    const featCampsite = campsites.campsitesArray.find((item) => item.featured);
+    const featPromotion = promotions.promotionsArray.find(
+        (item) => item.featured
+    );
+    const featPartner = partners.partnersArray.find((item) => item.featured);
 
     return (
         <ScrollView>
@@ -38,4 +46,6 @@ const HomeScreen = () => {
         </ScrollView>
     );
 };
+
 export default HomeScreen;
+
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,1 +1,3 @@
-export const baseUrl = 'http//10.0.0.55:30021/';
+export const baseUrl = 'http://10.0.0.55:3001/';
+
+// start json server: json-server -H 0.0.0.0 --watch db.json -p 3001 -d 2000
