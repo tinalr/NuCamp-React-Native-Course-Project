b4a759c0313cefec1d843e9f8047d99430e2b286
Christina Rontell <clrontell@gmail.com>
2022-07-30 08:47:10 -0500

    spacing, pencil icon, styles, modal local state

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -5,39 +5,39 @@ import { baseUrl } from '../shared/baseUrl';
 import Loading from '../components/LoadingComponent';
 
 const DirectoryScreen = ({ navigation }) => {
-    const campsites = useSelector((state) => state.campsites);
+  const campsites = useSelector((state) => state.campsites);
 
-    if (campsites.isLoading) {
-        return <Loading />;
-    }
-    if (campsites.errMess) {
-        return (
-            <View>
-                <Text>{campsites.errMess}</Text>
-            </View>
-        );
-    }
-
-    const renderDirectoryItem = ({ item: campsite }) => {
-        return (
-            <Tile
-                title={campsite.name}
-                caption={campsite.description}
-                featured
-                onPress={() =>
-                    navigation.navigate('CampsiteInfo', { campsite })
-                }
-                imageSrc={{ uri: baseUrl + campsite.image }}
-            />
-        );
-    };
+  if (campsites.isLoading) {
+    return <Loading />;
+  }
+  if (campsites.errMess) {
     return (
-        <FlatList
-            data={campsites.campsitesArray}
-            renderItem={renderDirectoryItem}
-            keyExtractor={(item) => item.id.toString()}
-        />
+      <View>
+        <Text>{campsites.errMess}</Text>
+      </View>
     );
+  }
+
+  const renderDirectoryItem = ({ item: campsite }) => {
+    return (
+      <Tile
+        title={campsite.name}
+        caption={campsite.description}
+        featured
+        onPress={() =>
+          navigation.navigate('CampsiteInfo', { campsite })
+        }
+        imageSrc={{ uri: baseUrl + campsite.image }}
+      />
+    );
+  };
+  return (
+    <FlatList
+      data={campsites.campsitesArray}
+      renderItem={renderDirectoryItem}
+      keyExtractor={(item) => item.id.toString()}
+    />
+  );
 };
 
 export default DirectoryScreen;
