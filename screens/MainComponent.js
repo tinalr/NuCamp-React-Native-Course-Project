edf0b2641e790f0a1e799003b59750594759f5d8
Christina Rontell <clrontell@gmail.com>
2022-08-12 15:48:11 -0500

    tab navigation

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -2,7 +2,7 @@ import { Image, Platform, StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants';
 import CampsiteInfoScreen from './CampsiteInfoScreen';
 import DirectoryScreen from './DirectoryScreen';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, HeaderTitle } from '@react-navigation/stack';
 import {
   createDrawerNavigator,
   DrawerContentScrollView,
@@ -22,6 +22,7 @@ import { fetchPromotions } from '../features/promotions/promotionsSlice';
 import { fetchComments } from '../features/comments/commentsSlice';
 import FavoritesScreen from './FavoritesScreen';
 import LoginScreen from './LoginScreen';
+import { getFocusedRouteNameFromRoute } from '@react-navigation/core';
 
 const Drawer = createDrawerNavigator();
 
@@ -151,15 +152,21 @@ const LoginNavigator = () => {
       <Stack.Screen
         name='Login'
         component={LoginScreen}
-        options={({ navigation }) => ({
+        options={({ navigation, route }) => ({
           headerLeft: () => (
             <Icon
-              name='sign-in'
+              name={
+                getFocusedRouteNameFromRoute(route) ===
+                  'Register'
+                  ? 'user-plus'
+                  : 'sign-in'
+              }
               type='font-awesome'
               iconStyle={styles.stackIcon}
               onPress={() => navigation.toggleDrawer()}
             />
-          )
+          ),
+          headerTitle: getFocusedRouteNameFromRoute(route)
         })}
       />
     </Stack.Navigator>
